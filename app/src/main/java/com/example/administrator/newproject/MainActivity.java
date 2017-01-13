8a52ed16bdc78cbe649db364baa1c8cb9d348fa3
Administrator <1084204954@qq.com>
2017-01-13 15:59:57 +0800

    第一次提交

--- app/src/main/java/com/example/administrator/newproject/MainActivity.java
+++ app/src/main/java/com/example/administrator/newproject/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //
-    //
+    //kjh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
